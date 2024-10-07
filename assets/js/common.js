dc51ce61c6399114bff17ca744fd9931769e6d67
amit-aromatic <rahulbhola.91@gmail.com>
2024-10-07 20:17:44 +0530

    auth url from js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,5 @@
 $( document ).ready(async function() {
+    setLoginLogoutUrl();
     const username = await cookieStore.get('username');
     if (username) showUsername(username);
 });
@@ -6,4 +7,12 @@ $( document ).ready(async function() {
 function showUsername(username) {
     $('#logout-nav').show();
     $('#login-nav').hide();
+}
+
+function setLoginLogoutUrl() {
+    const domain = 'https://login.goodhuman.in';
+    const client_id = '6npcn6fde0tucr6npeq625optk';
+    const scope = 'api%2Fall+aws.cognito.signin.user.admin+email+openid';
+    $('#login-url').prop('href', `${domain}/oauth2/authorize?client_id=${client_id}&response_type=code&scope=${scope}&redirect_uri=https%3A%2F%2Fthe.goodhuman.in%2F`);
+    $('#logout-url').prop('href', `${domain}/logout?client_id=${client_id}&response_type=code&scope=${scope}&logout_uri=https%3A%2F%2Fthe.goodhuman.in%2Flogout.html`);
 }
