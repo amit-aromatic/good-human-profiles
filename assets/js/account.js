da37d786d6451a335b86737c0db6a5b4690951fb
amit-aromatic <rahulbhola.91@gmail.com>
2024-10-13 12:07:52 +0530

    get account api

---
+++ assets/js/account.js
@@ -0,0 +1,24 @@
+$( document ).ready(async function() {
+    const token = await cookieStore.get('access_token');
+    if(token) getAccount(token);
+    else window.location.href = $('#login-url').prop('href');
+});
+
+async function getAccount(token) {
+    const settings = {
+        "url": "https://api.goodhuman.in/me",
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+          "Authorization": `Bearer ${token}`
+        },
+      };
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+        const username = response.filter(v => v.Name==='email')[0].Value;
+        const name = response.filter(v => v.Name==='name')[0].Value;
+        const birthday = response.filter(v => v.Name==='birthday')[0].Value;
+      });
+
+}
