e7b31304256deccad88926d26f630c9d17a07d0b
amit-aromatic <rahulbhola.91@gmail.com>
2024-10-15 13:24:43 +0530

    update user api

--- assets/js/account.js
+++ assets/js/account.js
@@ -1,10 +1,11 @@
+const token = $.cookie('access_token');
+
 $( document ).ready(async function() {
-    const token = typeof cookieStore !== 'undefined' ? await cookieStore.get('access_token') : $.cookie('access_token');
-    if(token) getAccount(token.value || token);
+    if(token) getAccount();
     else window.location.href = $('#login-url').prop('href');
 });
 
-async function getAccount(token) {
+async function getAccount() {
     const settings = {
         "url": "https://api.goodhuman.in/me",
         "method": "GET",
@@ -19,6 +20,25 @@ async function getAccount(token) {
         const username = response.filter(v => v.Name==='email')[0].Value;
         const name = response.filter(v => v.Name==='name')[0]?.Value;
         const birthday = response.filter(v => v.Name==='birthday')[0]?.Value;
+        $('#inputEmail').val(username);
+        $('#inputName').val(name);
       });
+}
 
+async function putAccount() {
+  const settings = {
+      "url": "https://api.goodhuman.in/me",
+      "method": "PUT",
+      "timeout": 0,
+      "headers": {
+        "Authorization": `Bearer ${token}`,
+        "Access-Control-Allow-Origin": window.location.origin
+      },
+      "data": JSON.stringify({ name: $('#inputName').val() })
+    };
+    
+    $.ajax(settings).done(function (response) {
+      console.log({ response })
+      getAccount();
+    });
 }
